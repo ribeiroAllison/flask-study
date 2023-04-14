024e3a0068bf7b4d74ed2cfdaaf97b0f76c31277
ribeiroAllison <allisonribeiro@gmail.com>
2023-04-14 11:28:48 -0300

    use filter in templates

--- app.py
+++ app.py
@@ -5,15 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  
-  #### Return a rendered index.html file
-  return render_template('index.html')
+  return render_template("index.html")
 
 @app.route("/recipe/<int:id>")
 def recipe(id):
-    
-  #### Return a rendered fried_egg.html file
-  return render_template('recipe.html',
-                        template_recipe = recipes[id],
-                        template_description=descriptions[id],
-                        template_ingredients=ingredients[id])
+  return render_template("recipe.html", 
+                        template_recipe=recipes[id], 
+                        template_ingredients=ingredients[id], 
+                        template_instructions=instructions[id])
